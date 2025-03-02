1e821aeb47bc6923c43bc272a0bf49e7392e1118
William Fernandes <2660859+10c8@users.noreply.github.com>
2025-03-02 16:24:18 -0300

    ver: 0.1.4 20250302

--- assets/Spinner-MXSLBe5O.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./auth-DCUkzX9t.js";import{o,c}from"./index-pTNjcZjp.js";const r={},n={class:"loader"};function s(t,_){return o(),c("div",n)}const f=e(r,[["render",s]]);export{f as S};
