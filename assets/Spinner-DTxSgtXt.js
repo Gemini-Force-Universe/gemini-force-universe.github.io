59023e1a27ef0b01132aa4c7e645c6be679c81ba
William Fernandes <2660859+10c8@users.noreply.github.com>
2025-08-05 01:34:18 -0300

    stock

---
+++ assets/Spinner-DTxSgtXt.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./auth-DHJhqjFq.js";import{c as o,o as c}from"./index-WG9b3cR3.js";const r={},n={class:"loader"};function s(t,_){return c(),o("div",n)}const f=e(r,[["render",s]]);export{f as S};
--- assets/Spinner-guPgTT7Q.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./auth-CLjzCcCX.js";import{c as o,o as c}from"./index--Lt7y2ja.js";const r={},n={class:"loader"};function s(t,_){return c(),o("div",n)}const f=e(r,[["render",s]]);export{f as S};
