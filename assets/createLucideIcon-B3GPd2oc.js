43f7e34bb39182dd71d87c29720b3373832204ec
William Fernandes <2660859+10c8@users.noreply.github.com>
2026-01-15 09:05:45 -0300

    update

--- assets/createLucideIcon-B3GPd2oc.js
+++ assets/createLucideIcon-B3GPd2oc.js
@@ -1,4 +1,4 @@
-import{ac as a}from"./index-BeIQgWCZ.js";/**
+import{ac as a}from"./index-BQFS1Hb1.js";/**
  * @license lucide-vue-next v0.429.0 - ISC
  *
  * This source code is licensed under the ISC license.
