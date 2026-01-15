cd7f8a399b1fe5c965ef14af415f3e6ebc2fbc9e
William Fernandes <2660859+10c8@users.noreply.github.com>
2026-01-15 13:01:15 -0300

    fixes

---
+++ assets/Spinner-BrnJNbLi.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./auth-C_notfj6.js";import{c as o,o as c}from"./index-BHGPtDt-.js";const r={},n={class:"loader"};function s(t,_){return c(),o("div",n)}const f=e(r,[["render",s]]);export{f as S};
--- assets/createLucideIcon-C_4rDVLA.js
+++ assets/createLucideIcon-C_4rDVLA.js
@@ -1,4 +1,4 @@
-import{ac as a}from"./index-DCSyfbzt.js";/**
+import{ac as a}from"./index-BHGPtDt-.js";/**
  * @license lucide-vue-next v0.429.0 - ISC
  *
  * This source code is licensed under the ISC license.
