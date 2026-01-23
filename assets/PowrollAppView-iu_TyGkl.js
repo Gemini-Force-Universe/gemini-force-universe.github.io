be1c45e9b0d933741047fee6cbaf5a0bdd64f441
William Fernandes <2660859+10c8@users.noreply.github.com>
2026-01-23 16:01:50 -0300

    upgrade

--- assets/PowrollAppView-iu_TyGkl.js
+++ assets/PowrollAppView-iu_TyGkl.js
@@ -1,4 +1,4 @@
-import{C as U,d as z,r as T,a as f,q as V,c as m,o as p,g as o,y as l,u as s,x as N,f as u,w as b,z as x,A as c,L as h,a6 as j,aN as D,J as P,K as A}from"./index-BdIGBS6c.js";import{a as y,_ as k}from"./Label.vue_vue_type_script_setup_true_lang-oL9ZJ_WV.js";import{c as a}from"./utils-CytzSlOG.js";import{u as R,C as E,E as I}from"./auth-DqyZRKXX.js";import{c as L}from"./createLucideIcon-DGWsdROY.js";import"./index-DkqMKDLA.js";import"./index-BwobEAja.js";/**
+import{C as U,d as z,r as T,a as f,q as V,c as m,o as p,g as o,y as l,u as s,x as N,f as u,w as b,z as x,A as c,L as h,a6 as j,aN as D,J as P,K as A}from"./index-ChkyK8dU.js";import{a as y,_ as k}from"./Label.vue_vue_type_script_setup_true_lang-CJUm3wTM.js";import{c as a}from"./utils-CytzSlOG.js";import{u as R,C as E,E as I}from"./auth-C5TtoZlX.js";import{c as L}from"./createLucideIcon-Ds1Nq8Db.js";import"./index-DN3z7IlZ.js";import"./index-BwobEAja.js";/**
  * @license lucide-vue-next v0.429.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/createLucideIcon-Ds1Nq8Db.js
+++ assets/createLucideIcon-Ds1Nq8Db.js
@@ -1,4 +1,4 @@
-import{ac as a}from"./index-BdIGBS6c.js";/**
+import{ac as a}from"./index-ChkyK8dU.js";/**
  * @license lucide-vue-next v0.429.0 - ISC
  *
  * This source code is licensed under the ISC license.
